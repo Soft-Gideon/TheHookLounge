f724ecc054adbb78f333369abdff5d0c1f67ad4d
gideon <gideonsunday.dev@gmail.com>
2024-08-29 18:58:44 +0100

    added landing page

---
+++ main.js
@@ -0,0 +1,48 @@
+const navigation = document.querySelector('.navigation');
+
+const btns = document.querySelectorAll('.nav-btn');
+const slides = document.querySelectorAll('.img-slide');
+const contents = document.querySelectorAll('.content');
+
+function sliderNav(manual) {
+  btns.forEach((btn) => {
+    btn.classList.remove('active');
+  });
+
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+  });
+
+  contents.forEach((content) => {
+    content.classList.remove('active');
+  })
+
+  btns[manual].classList.add('active');
+  slides[manual].classList.add('active');
+  contents[manual].classList.add('active');
+}
+
+let index = 0;
+  btns.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+      clearInterval(setIntervalId);
+      index = i;
+      sliderNav(index);
+      console.log(index);
+      interval();
+    });
+  });
+
+  let setIntervalId;
+  function interval() {
+  setIntervalId = setInterval(() => {
+    if (index >= btns.length) {
+      index = 0;
+    }
+    sliderNav(index);
+    index++
+  }, 5000);
+}
+
+// interval();
+window.onload = interval;
