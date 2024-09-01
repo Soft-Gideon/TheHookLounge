07770598a94c54e33040bdd543b6ff9fffa2772c
Soft-Gideon <gideonsunday.dev@gmail.com>
2024-09-01 23:24:22 +0100

    Added gallery section

---
+++ script.js
@@ -0,0 +1,66 @@
+// vvv Cards Sliders vvv
+const  wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children]
+
+let isDragging = false, startX, startScrollingLeft, timeOutId;
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+  carousel.insertAdjacentHTML("afterbegin", card.outerHTML)
+});
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+  carousel.insertAdjacentHTML("beforeend", card.outerHTML)
+});
+
+
+// Scroll card eventlistener
+arrowBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+  });
+});
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  startX = e.pageX;
+  startScrollingLeft = carousel.scrollLeft;
+}
+const dragging = (e)  => {
+  if (!isDragging) return;
+  carousel.scrollLeft = startScrollingLeft - (e.pageX - startX);
+}
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+}
+
+const autoPlay = () => {
+  // if (window.innerWidth < 800) return;
+  timeOutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
+}
+autoPlay();
+
+const infiniteScroll = () => {
+  if (carousel.scrollLeft === 0) {
+    carousel.classList.add('no-transition');
+    carousel.scrollLeft = carousel.scrollWidth - ( 2 * carousel.offsetWidth );
+    carousel.classList.remove('no-transition');
+  } else if (Math.round(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+    carousel.classList.add('no-transition');
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.remove('no-transition');
+  }
+
+  clearTimeout(timeOutId);
+  if (!wrapper.matches(":hover")) autoPlay();
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+document.addEventListener("mouseenter",console.log('mouse enter'), clearTimeout(timeOutId));
+carousel.addEventListener("mouseleave", autoPlay);
