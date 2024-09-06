0b2596a04107b714a57468f52bfe24e05fc67921
Soft-Gideon <gideonsunday.dev@gmail.com>
2024-09-06 03:15:36 +0100

    Added responsive menu

--- main.js
+++ main.js
@@ -1,57 +1,54 @@
-const navigation = document.querySelector('.navigation');
-
-const btns = document.querySelectorAll('.nav-btn');
-const slides = document.querySelectorAll('.img-slide');
-const contents = document.querySelectorAll('.content');
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".img-slide");
+const contents = document.querySelectorAll(".content");
 
 function sliderNav(manual) {
   btns.forEach((btn) => {
-    btn.classList.remove('active');
+    btn.classList.remove("active");
   });
 
   slides.forEach((slide) => {
-    slide.classList.remove('active');
+    slide.classList.remove("active");
   });
 
   contents.forEach((content) => {
-    content.classList.remove('active');
-  })
+    content.classList.remove("active");
+  });
 
-  btns[manual].classList.add('active');
-  slides[manual].classList.add('active');
-  contents[manual].classList.add('active');
+  btns[manual].classList.add("active");
+  slides[manual].classList.add("active");
+  contents[manual].classList.add("active");
 }
 
 let index = 0;
-  btns.forEach((btn, i) => {
-    btn.addEventListener('click', () => {
-      clearInterval(setIntervalId);
-      index = i;
-      sliderNav(index);
-      interval();
-    });
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    clearInterval(setIntervalId);
+    index = i;
+    sliderNav(index);
+    interval();
   });
+});
 
-  let setIntervalId;
-  function interval() {
+let setIntervalId;
+function interval() {
   setIntervalId = setInterval(() => {
     if (index >= btns.length) {
       index = 0;
     }
     sliderNav(index);
-    index++
+    index++;
   }, 5000);
 }
 
 // interval();
 window.onload = interval;
 
-
 // scroll Button Effect
 const main = document.querySelector("main");
 const scrollTop = document.querySelector(".scroll-top");
 const header = document.querySelector("header");
- scrollTop.addEventListener("click", () => {
+scrollTop.addEventListener("click", () => {
   main.scrollTo({ top: 0, behavior: "smooth" });
 });
 const scrolling = () => {
@@ -66,5 +63,32 @@ const scrolling = () => {
   } else {
     header.classList.remove("active");
   }
-}
+};
 main.addEventListener("scroll", scrolling);
+
+// menu
+const menu = document.querySelector(".menu");
+const navigation = document.querySelector(".navigation");
+const closeMenu = document.querySelector(".navigation-items .close");
+
+const navigationItems = document.querySelector(".navigation-items");
+
+menu.addEventListener("click", () => {
+  menu.classList.add("active");
+  navigation.classList.add("active");
+});
+closeMenu.addEventListener("click", () => {
+  menu.classList.remove("active");
+  navigation.classList.remove("active");
+});
+document.addEventListener("click", (e) => {
+  if (!navigationItems.contains( e.target) && !menu.contains( e.target)) {
+    menu.classList.remove("active");
+    navigation.classList.remove("active");
+  }
+});
+
+window.addEventListener("resize", () => {
+  menu.classList.remove("active");
+  navigation.classList.remove("active");
+});
